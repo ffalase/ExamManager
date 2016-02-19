a6d1bf9e816b39f71dcf1a5c801269fca3a2a112
Femi Falase <femi.falase@gmail.com>
2016-02-19 21:21:01 +0100

    Project baseline

---
+++ src/com/falase/exammanager/exceptions/DuplicateAliasException.java
@@ -0,0 +1,12 @@
+package com.falase.exammanager.exceptions;
+
+/**
+ * Created by Femi Falase on 19/02/2016.
+ */
+public class DuplicateAliasException extends Exception {
+
+
+    public DuplicateAliasException(String message) {
+        super(message);
+    }
+}
